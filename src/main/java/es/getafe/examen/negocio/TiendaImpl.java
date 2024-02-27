76b96536d810b385642c9566cf47e8c72900d52e
Mañanas <Maanas@host.docker.internal>
2024-02-27 12:17:13 +0100

    añadiendo test

--- src/main/java/es/getafe/examen/negocio/TiendaImpl.java
+++ src/main/java/es/getafe/examen/negocio/TiendaImpl.java
@@ -1,5 +1,67 @@
 package es.getafe.examen.negocio;
 
-public class TiendaImpl {
+import java.util.Set;
+
+import es.getafe.examen.modelo.Fabricante;
+import es.getafe.examen.modelo.Producto;
+
+public class TiendaImpl implements Tienda{
+
+	private 
+	
+	@Override
+	public Set<Producto> getProductos() {
+		
+		
+		return null;
+	}
+
+	@Override
+	public Set<Producto> getProductos(String descripcion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getMediaPrecioProductosByFabricante(int idFabricante) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void addFabricante(Fabricante fabricante) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addProducto(Producto producto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<Fabricante> getFabricantes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Fabricante> getFabricantesActivos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Fabricante getFabricante(int idFabricante) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Fabricante getFabricanteConProductos(int idFabricante) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
